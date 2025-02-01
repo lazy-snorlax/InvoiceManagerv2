09c44bb935d70e7540209249833c8909153e1df0
Anthony <blacknebula236@gmail.com>
2025-02-01 23:03:36 +1030

    add card to view

--- frontend/src/views/Customers/CustomerList.tsx
+++ frontend/src/views/Customers/CustomerList.tsx
@@ -5,10 +5,16 @@ type Props = {}
 const CustomerList = (props: Props) => {
     return (
         <>
-            <h2>This is a list of customers</h2>
-            <div className="">
-                <Link className="btn btn-neutral w-50" to="/customers/01">Look at a Customer</Link>
+        <div className="container mx-auto mt-4">
+            <div className="card bg-base-300 w-full">
+                <div className="card-body items-center text-center">
+                    <h2>This is a list of customers</h2>
+                    <div className="">
+                        <Link className="btn btn-neutral w-50" to="/customers/01">Look at a Customer</Link>
+                    </div>
+                </div>
             </div>
+        </div>
         </>
     )
 }
