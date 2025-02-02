c1418424ce8d3527162f99f00464dd2076e4e573
Anthony <blacknebula236@gmail.com>
2025-02-02 17:46:07 +1030

    css adjustment

--- frontend/src/components/Invoice/InvoiceTransLines.jsx
+++ frontend/src/components/Invoice/InvoiceTransLines.jsx
@@ -4,22 +4,20 @@ import InvoiceTransLine from "./InvoiceTransLine";
 
 const InvoiceTransLines = (props) => {
   return (
-    <>
-      <div className="overflow-x-auto h-64">
-        <div className="grid grid-cols-11 grid-rows-1 gap-2">
-          <div className="my-auto">Item No</div>
-          <div className="my-auto col-span-5 col-start-2">Description</div>
-          <div className="my-auto ">Tax%</div>
-          <div className="my-auto ">GST</div>
-          <div className="my-auto ">Cost</div>
-          <div className="my-auto ">Expense</div>
-          <button className="btn btn-primary" onClick={() => { console.log("new transaction line btn clicked") }}>+</button>
-        </div>
-        {props.lines.map((line, index) => (
-          <InvoiceTransLine line={line} key={`line-${index}`} />
-        ))}
+    <div className="overflow-x-auto h-64">
+      <div className="grid grid-cols-11 grid-rows-1 gap-2">
+        <div className="my-auto">Item No</div>
+        <div className="my-auto col-span-5 col-start-2">Description</div>
+        <div className="my-auto ">Tax%</div>
+        <div className="my-auto ">GST</div>
+        <div className="my-auto ">Cost</div>
+        <div className="my-auto ">Expense</div>
+        <button className="btn btn-primary text-2xl" onClick={() => { console.log("new transaction line btn clicked") }}>+</button>
       </div>
-    </>
+      {props.lines.map((line, index) => (
+        <InvoiceTransLine line={line} key={`line-${index}`} />
+      ))}
+    </div>
   );
 }
 
