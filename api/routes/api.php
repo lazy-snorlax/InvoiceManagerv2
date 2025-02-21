c8e35c8c9cf5096d3a2a5fe4c39474c8ee83a621
Anthony <blacknebula236@gmail.com>
2025-02-21 23:53:07 +1030

    move routes into sanctum middleware

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,23 +18,25 @@ Route::post('/password/forgot', Controllers\Password\ForgotPasswordController::c
 Route::post('/password/reset', Controllers\Password\ResetPasswordController::class);
 
 
-// TODO: Lookups =====================================================
-Route::get('/invoices', [Controllers\InvoiceController::class, 'index']);
-Route::get('/invoices/{id}', [Controllers\InvoiceController::class, 'show']);
-Route::get('/invoice-list', Controllers\InvoiceListController::class);
-
-Route::get('/quotes', [Controllers\QuoteController::class, 'index']);
-Route::get('/quotes/{id}', [Controllers\QuoteController::class, 'show']);
-Route::get('/quote-list', Controllers\QuoteListController::class);
-
-Route::get('/companies', [Controllers\CompanyController::class, 'index']);
-Route::get('/companies/{id}', [Controllers\CompanyController::class, 'show']);
-Route::get('/company-list', Controllers\CompanyListController::class);
-
-
 // Authenticated ============================================================
 Route::middleware(['auth:sanctum'])->group(function () {
+    
     Route::get('/user', Controllers\LoggedInController::class);
     Route::put('/user', Controllers\UpdateAccountDetailsController::class);
     Route::put('/user/password', Controllers\UpdatePasswordController::class);
+
+    
+    // TODO: Lookups =====================================================
+    Route::get('/invoices', [Controllers\InvoiceController::class, 'index']);
+    Route::get('/invoices/{id}', [Controllers\InvoiceController::class, 'show']);
+    Route::get('/invoice-list', Controllers\InvoiceListController::class);
+    
+    Route::get('/quotes', [Controllers\QuoteController::class, 'index']);
+    Route::get('/quotes/{id}', [Controllers\QuoteController::class, 'show']);
+    Route::get('/quote-list', Controllers\QuoteListController::class);
+    
+    Route::get('/companies', [Controllers\CompanyController::class, 'index']);
+    Route::get('/companies/{id}', [Controllers\CompanyController::class, 'show']);
+    Route::get('/company-list', Controllers\CompanyListController::class);
+    
 });
