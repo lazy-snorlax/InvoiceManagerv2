262be2510d0d19326242338ce1991c333cf3f62a
Anthony <blacknebula236@gmail.com>
2025-02-23 17:48:21 +1030

    add PUT request for invoice

--- api/app/Http/Controllers/InvoiceController.php
+++ api/app/Http/Controllers/InvoiceController.php
@@ -23,4 +23,46 @@ class InvoiceController extends Controller
         abort_if($id->type != 1, 500, 'This is not an invoice!');
         return new InvoiceResource($id->load('headers.lines'));
     }
+
+    public function store(Request $request)
+    {
+        dd("create new invoice");
+    }
+
+    public function update(Request $request, $id) 
+    {
+        $invoice = TransactionMain::findOrFail($id);
+        $invoice->update($request->only([
+            'business_no',
+            'company_no',
+            'order_no',
+            'note',
+            'created_at',
+        ]));
+        
+        // Headers - update or create
+        foreach ($request->input('transactions') as $headerData) {
+            $header = $invoice->headers()->updateOrCreate(
+                ['id' => array_key_exists("id", $headerData) ? $headerData['id'] : null],
+                ['transaction_main_id' => $invoice['id']],
+                ['description' => $headerData['description']],
+            );
+
+            // Lines - update or create
+            foreach ($headerData['lines'] as $lineData) {
+                $header->lines()->updateOrCreate(
+                    ['id' => array_key_exists("id", $lineData) ? $lineData['id'] : null],
+                    ['transaction_header_id' => $header['id']],
+                    ['description' => $lineData['description']],
+                    ['item' => $lineData['item']],
+                    ['tax' => $lineData['tax'] / 100],
+                    ['gst' => array_key_exists("gst", $lineData) ? $lineData['gst'] : $lineData['cost'] * ($lineData['tax'] / 100)],
+                    ['cost' => $lineData['cost']],
+                    ['expense' => $lineData['expense']],
+                );
+            }
+        }
+        
+        return new InvoiceResource($invoice->load(['headers.lines']));
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -29,6 +29,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // TODO: Lookups =====================================================
     Route::get('/invoices', [Controllers\InvoiceController::class, 'index']);
     Route::get('/invoices/{id}', [Controllers\InvoiceController::class, 'show']);
+    Route::put('/invoices/{id}', [Controllers\InvoiceController::class, 'update']);
     Route::get('/invoice-list', Controllers\InvoiceListController::class);
     
     Route::get('/quotes', [Controllers\QuoteController::class, 'index']);
