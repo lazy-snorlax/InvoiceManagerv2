7012ffb247f941ee5a383746f37f23fb62b642e4
Anthony <blacknebula236@gmail.com>
2025-03-04 19:32:52 +1030

    add toast container to root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { UserProvider } from './context/useAuth'
 import Sidebar from './components/Sidebar'
+import { ToastContainer, Bounce } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -12,7 +14,19 @@ function App() {
           <div className="drawer-content flex flex-col">
             <Navbar />
             <Outlet />
-            <Sidebar/>
+            <Sidebar />
+            <ToastContainer 
+              position="bottom-right"
+              autoClose={4000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+              transition={Bounce} />
           </div>
         </div>
       </UserProvider>
